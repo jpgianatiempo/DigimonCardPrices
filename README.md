de5b67a5496dfc0b60036fef950548f92b0a6a62
J. P. Giana <juanp.giana@gmail.com>
2021-12-20 15:36:05 -0300

    readme+script
    Signed-off-by: J. P. Giana <juanp.giana@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DigimonCardPrices
+
+Un amigo necesitaba tener los precios actualizados de todas las cartas (singles) de Digimon. El punto es que el hacia todo el proceso a mano y las iba cargando en su respectiva página. Pero encontramos que podía cargar un csv/xlsx con el precio y eso aumentaría notablemente su eficiencia.
+
+Además, como su página es de Argentina y los precios de las cartas de Troll and Toad están en íngles había que conseguir un tipo de cambio paralelo que le garantice la reposición de sus productos.
+
+Por este motivo le cree un código que recorre el listado de cartas publicadas en Troll and Toad y obtiene de cada una el precio y su nombre. Hubo que prestar atención porque cuando una carta no tenía stock, el precio estaba alojado en otro lugar de la página. No obstante, existía un contenedor que tenía todos los datos necesarios.
+
+Una vez que se resolvió eso, solo restaba conseguir un tipo de cambio que sea representativo del valor de mercado (no el tipo de cambio oficial). Entonces encontré una página que publica el precio del dólar paralelo (blue) y lo scrapea para obtener los precios de las cartas en pesos.
---
+++ scrap_digi_prices.py
@@ -0,0 +1,60 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+
+#genero lista de cantidad de páginas
+#pongo un número exagerado para que dure por mucho tiempo el scrap sin modificarlo, actualmente son 20 pág
+paginas = list(range(10000))
+#lista para nombres
+nombres = []
+#lista para precios
+precios = []
+#for loop para ir scrapeando por cada página
+for pagina in paginas:
+    try:
+        r = requests.get("https://www.trollandtoad.com/final-fantasy-more-ccgs/all-digimon-singles/17909?Keywords=&min-price=&max-price=&items-pp=60&item-condition=&selected-cat=17909&sort-order=&page-no="+str(pagina)+"&view=grid&subproduct=0")
+        soup = BeautifulSoup(r.text, "html.parser")
+        #selecciono el contenedor que tiene nombre y precio de la carta
+        total = soup.findAll("div", {"class":"product-info card-body col pl-0 pl-sm-3"})
+        #agrego esta linea para que salte IndexError si el número de página no contiene info y corte el loop
+        total[0].text
+        #por cada carta en cada pagina
+        for carta in total :
+            #selecciono nombre
+            nombre = carta.text.split("\n")[1]
+
+            try:
+                #precio de la carta si esta en stock
+                precio_stock = carta.text.split("\n")[6].split("$")[1]
+            except IndexError:
+                try:
+                    #precio de la carta si no esta en stock, xq esta en un lugar distinto
+                    precio_stock = carta.text.split("\n")[2].split("$")[1]
+                except IndexError:
+                    #en caso de que no haya ninguno (no deberia) le pone NAN
+                    precio_stock = np.NaN
+
+            nombres.append(nombre)
+            precios.append(precio_stock)
+    except IndexError:
+        break
+
+#junto todo en el dataframe
+df = pd.DataFrame(list(zip(nombres, precios)),
+               columns =['Carta', 'Precio_Usd'])
+
+#pasar precio a número
+df.Precio_Usd = pd.to_numeric(df.Precio_Usd)
+
+#scrap precio dolar blue o setear dolar a gusto!
+re = requests.get("https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB")
+sopa = BeautifulSoup(re.text, "html.parser")
+dolar_vendedor = sopa.findAll("div", {"class":"sell-value"},{"class":"currency"})
+dolar = pd.to_numeric(dolar_vendedor[0].text.strip("$").replace(",","."))
+
+#convertir a pesos
+df["Precio_Pesos"] = df.Precio_Usd * dolar
+
+#grabo el excel
+df.to_excel("Precios_Digimon.xlsx")
